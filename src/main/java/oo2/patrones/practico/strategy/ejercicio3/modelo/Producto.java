8990133b279c7e62ac187bf5e2aae7473164fa7a
Andrés Choque Lopez <andreschoquelopez@gmail.com>
2024-06-19 04:00:50 -0300

    Ejercicio3: Cada valor del enum TipoProducto tiene una instancia de CalculadorDePrecios. El método Producto.precioFinal llamará a los métodos de CalculadorDePrecios obtenido del enum.

--- src/main/java/oo2/patrones/practico/strategy/ejercicio3/modelo/Producto.java
+++ src/main/java/oo2/patrones/practico/strategy/ejercicio3/modelo/Producto.java
@@ -10,42 +10,8 @@ public class Producto {
 	}
 
 	public double precioFinal() {
-		double impuestos = 0;
-		double descuentos = 0;
-		boolean envioGratis = false;
-		if (tipo == TipoProducto.LIBRO) {
-			impuestos = 0.1;
-			descuentos = 0.1;
-			if (precio > 100) {
-				envioGratis = true;
-			}
-		} else if (tipo == TipoProducto.ALIMENTO) {
-			impuestos = 0.05;
-			if (precio > 100) {
-				descuentos = 0.15;
-			}
-			if (precio > 200) {
-				envioGratis = true;
-			}
-		} else if (tipo == TipoProducto.MEDICINA) {
-			impuestos = 0;
-			if (precio > 50) {
-				descuentos = 0.1;
-			}
-			if (precio > 100) {
-				envioGratis = true;
-			}
-		} else {
-			impuestos = 0.15;
-			if (precio > 50) {
-				descuentos = 0.05;
-			}
-			if (precio > 200) {
-				envioGratis = true;
-			}
-		}
-		double total = precio * (1 + impuestos) * (1 - descuentos);
-		if (envioGratis) {
+		double total = tipo.getCalculador().calcularPrecio(precio);
+		if (tipo.getCalculador().tieneEnvioGratis(precio)) {
 			total -= 10;
 		}
 		return total;
--- src/main/java/oo2/patrones/practico/strategy/ejercicio3/modelo/TipoProducto.java
+++ src/main/java/oo2/patrones/practico/strategy/ejercicio3/modelo/TipoProducto.java
@@ -1,8 +1,23 @@
 package oo2.patrones.practico.strategy.ejercicio3.modelo;
 
+import oo2.patrones.practico.strategy.ejercicio3.modelo.calculador.Alimento;
+import oo2.patrones.practico.strategy.ejercicio3.modelo.calculador.Libro;
+import oo2.patrones.practico.strategy.ejercicio3.modelo.calculador.Medicina;
+import oo2.patrones.practico.strategy.ejercicio3.modelo.calculador.Otro;
+
 public enum TipoProducto {
-	LIBRO,
-	ALIMENTO,
-	MEDICINA,
-	OTRO
+	LIBRO(new Libro()),
+	ALIMENTO(new Alimento()),
+	MEDICINA(new Medicina()),
+	OTRO(new Otro());
+
+	private final CalculadorDePrecios calculador;
+
+	TipoProducto(CalculadorDePrecios calculador) {
+		this.calculador = calculador;
+	}
+
+	CalculadorDePrecios getCalculador() {
+		return calculador;
+	}
 }
